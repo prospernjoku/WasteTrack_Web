12532e9d284d5abe1fc9554f4d0a822211898f43
Prosper Njoku <prospernjoku@gmail.com>
2024-02-23 15:58:27 +0100

    mainwork
    mainwork

---
+++ precache-manifest.f1bd27d20bbea0b2c76cdfda575025b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06cb29487dbb067fabaf560843a62726",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d61623e910c8c7947502",
+    "url": "/static/css/2.6f2db2d8.chunk.css"
+  },
+  {
+    "revision": "09e79804d2ccac9c2a2e",
+    "url": "/static/css/main.c381a88d.chunk.css"
+  },
+  {
+    "revision": "d61623e910c8c7947502",
+    "url": "/static/js/2.5ad84b1c.chunk.js"
+  },
+  {
+    "revision": "c57c6973a217b4a157a9a83354c16540",
+    "url": "/static/js/2.5ad84b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09e79804d2ccac9c2a2e",
+    "url": "/static/js/main.377606d2.chunk.js"
+  },
+  {
+    "revision": "be670501c177ba4aea9f",
+    "url": "/static/js/runtime-main.850873e3.js"
+  }
+]);
